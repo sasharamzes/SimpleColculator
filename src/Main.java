8ffdb58eb19589ceaaedf268271c634c41d1c04c
sasharamzes <sasharamzes@gmail.com>
2023-05-26 11:32:19 +0500

    add pw.close()

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,19 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException{
 
         File file = new File("input");
-        File file1 = new File("output");
+        File file1 = new File("output.txt");
         PrintWriter pw = new PrintWriter(file1);
         Scanner sc = null;
         try {
             sc = new Scanner(file);
         } catch (FileNotFoundException e) {
             pw.println("File not found");
+            pw.close();
         }
         double a = 0;
         double b = 0;
         boolean flag = true;
 
-        sc.useLocale(Locale.US);
         String str = sc.nextLine();
         String[] arr = str.split(" ");
         try {
@@ -29,24 +29,29 @@ public class Main {
             b = Double.parseDouble(arr[2]);
         } catch (NumberFormatException e) {
             pw.println("Error! Not number");
+            pw.close();
             flag = false;return;
         }
         if("/*-+".contains(arr[1]) && flag){
             switch (arr[1]){
                 case "+":{
-                    pw.println(a + b);break;}
+                    pw.println(a + b);
+                    pw.close();break;}
+
                 case "-":{
-                    pw.println(a - b);break;
+                    pw.println(a - b);pw.close();break;
                 }
                 case "*":{
-                    pw.println(a * b);break;
+                    pw.println(a * b);pw.close();break;
                 }
                 case "/":{
                     try {
-                        if(b == 0.0)
+                        if(b == 0.0){
                         pw.println("Error! Division by zero");
+                        pw.close();}
                         else
                         pw.println(a / b);
+                        pw.close();
                     } catch (ArithmeticException e) {
 
                     }break;
@@ -54,5 +59,6 @@ public class Main {
             }
         }else
             pw.println("Operation Error!");
+        pw.close();
     }
 }
